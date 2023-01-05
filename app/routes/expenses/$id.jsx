0e5bbac5aa8f59dc8098312a3674817969edb0bc
Philip <philip.alexander.hassialis@gmail.com>
2023-01-06 00:31:15 +0200

    Rendering a Modal Page (Overlay Page)

--- app/routes/expenses/$id.jsx
+++ app/routes/expenses/$id.jsx
@@ -1,7 +1,12 @@
 import ExpenseForm from "~/components/expenses/ExpenseForm";
+import Modal from "~/components/util/Modal";
 
 const UpdateExpensesPage = () => {
-  return <ExpenseForm />;
+  return (
+    <Modal>
+      <ExpenseForm />
+    </Modal>
+  );
 };
 
 export default UpdateExpensesPage;
--- app/routes/expenses/add.jsx
+++ app/routes/expenses/add.jsx
@@ -1,7 +1,12 @@
 import ExpenseForm from "~/components/expenses/ExpenseForm";
+import Modal from "~/components/util/Modal";
 
 const AddExpensesPage = () => {
-  return <ExpenseForm />;
+  return (
+    <Modal>
+      <ExpenseForm />
+    </Modal>
+  );
 };
 
 export default AddExpensesPage;
